309d0a8d3dfa7c571032e58255bc356ae39de980
Momna Ijaz <momna533gb@gmail.com>
2024-05-31 08:49:01 +0500

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,7 +20,7 @@ function Contact() {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/contact",
+        "https://portfolio-1-contact-api.vercel.app/",
         formData,
         {
           headers: {
