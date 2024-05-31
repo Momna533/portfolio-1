042b05755d5d1a31e9efd4f2fa58410d490c1b88
Momna Ijaz <momna533gb@gmail.com>
2024-05-31 07:34:02 +0500

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,7 +20,7 @@ function Contact() {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:5000/submit/submit",
+        "http://localhost:5000/submit",
         formData,
         {
           headers: {
